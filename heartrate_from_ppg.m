518f557b795010bd82629ae6fd5bf8116d98c091
Pratik Jain <pratik.jain0509@gmail.com>
2018-04-19 18:55:13 +0530

    Added Error calculation
    The average value obtained by both the signals has lower squared error.

--- heartrate_from_ppg.m
+++ heartrate_from_ppg.m
@@ -1,3 +1,7 @@
+close all;
+
+load './DATA_01_TYPE01.mat';
+load 'DATA_01_TYPE01_BPMtrace.mat';
 data = sig;
 
 I = data(2:end, :)';
@@ -8,11 +12,10 @@ fmax = 2.5;
 tmax = 5 * 60;
 delt = 8;
 step = 2;
-s = [0:2:(tmax - delt + step)];
+s = 0:2:(tmax - delt + step);
 s = (s * Fs) + 1;
 dist = delt * Fs;
 dilation = 64 * Fs * 16;
-
 
 Y1 = [];
 Y2 = [];
@@ -31,12 +34,12 @@ for i = s
     
     temp = FFTPPG1(first:last, :);
     temp = abs(temp);
-    [x, peak_ppg1] = max(temp);
+    [~, peak_ppg1] = max(temp);
     peak_ppg1 = peak_ppg1 + first;
     
-    temp = FFTPPG1(first:last, :);
+    temp = FFTPPG2(first:last, :);
     temp = abs(temp);
-    [x, peak_ppg2] = max(temp);
+    [~, peak_ppg2] = max(temp);
     peak_ppg2 = peak_ppg2 + first;
     
     fppg1 = peak_ppg1 * Fs / dilation;
@@ -46,4 +49,17 @@ for i = s
     HR2 = fppg2 * 60;
     Y1 = [Y1; HR1];
     Y2 = [Y2; HR2];
+    Yavg = (Y1 + Y2) / 2;
 end
+
+err_hr1 = Y1 - BPM0;
+err_hr1 = err_hr1 .^ 2;
+sum(err_hr1)
+
+err_hr2 = Y2 - BPM0;
+err_hr2 = err_hr2 .^ 2;
+sum(err_hr2)
+
+err_hravg = Yavg - BPM0;
+err_hravg = err_hravg .^ 2;
+sum(err_hravg)
